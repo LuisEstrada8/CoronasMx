52701421510a1a1f52ea6a7a0fcd86e06847bf55
Luis <estrada.villegas.17071@itsmante.edu.mx>
2020-11-26 00:38:57 -0600

    MigraciionCompleta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ForoComponent } from './Components/foro/foro.component';
 import { HomeComponent } from './Components/home/home.component';
 import { APP_ROUTING } from './app.routes';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
@@ -12,10 +13,11 @@ import { APP_ROUTING } from './app.routes';
     AppComponent,
     ForoComponent,
     HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
